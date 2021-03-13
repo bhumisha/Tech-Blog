f2e198d548d70a5f9528936d01a35eff89edfe54
Bhumisha Dave <bhumishadave@yahoo.com>
2021-03-13 11:20:33 -0800

    Initial Setup with working code

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,20 @@
+const { Comment } = require('../models');
+
+const commentdata = [
+  
+  {
+    comment_text: 'Aliquam erat volutpat. In congue.',
+    user_id: 1,
+    post_id: 1
+  },
+  {
+    comment_text: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    user_id: 1,
+    post_id: 1
+  },
+
+];
+
+const seedComments = () => Comment.bulkCreate(commentdata);
+
+module.exports = seedComments;
---
+++ seeds/user-seeds.js
@@ -0,0 +1,13 @@
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+
+const userdata = [
+  {
+    username: 'user1',
+    password: 'password123'
+  }
+];
+
+const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
+
+module.exports = seedUsers;
